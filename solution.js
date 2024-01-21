964943c0d82fdcf31caa6a644d53c1e24252b2f9
Nicole Slater <nicoleslater@pursuit.org>
2024-01-21 15:02:55 -0500

    Updating Stack methods

--- solution.js
+++ solution.js
@@ -1,6 +1,38 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+  constructor(data, next = null){
+    this.data = data;
+    this.next = next;
+  }
+}
+class Stack {
+  constructor(){
+    this.top = null;
+  }
+
+count(){
+  return this.size;
+}
+
+dequeue(){
+  if(this.top === null){
+    return "Empty Queue!";
+  }
+
+  
+}
+
+push(data){
+    const newNode = newNode(data);
+    newNode.next = this.top;
+    this.top = newNode;
+
+    this.size++
+  }
+}
+
 module.exports = {
   Node,
   Queue,
